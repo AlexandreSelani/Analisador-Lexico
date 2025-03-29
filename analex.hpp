12228f1f7fcf0e64597d24f9c99aa5e18e4d6aec
Alexandre Selani <xandyselani@outlook.com>
2025-03-29 14:16:35 -0300

    ajuste dos headers

--- analex.hpp
+++ analex.hpp
@@ -1,21 +1,34 @@
 #include <string>
 #include <fstream>
+#include <unordered_map>
 class Analex{
     private:
         std::string atomo;
         char proximo;
         int contadorLinhas;
         int contadorDeIdentificadores;
-
+        std::unordered_map<std::string, std::string> tabelaDeSimbolos;
+        std::unordered_map<std::string, int> tabelaDeIdentificadores;
         std::ifstream programaFonte;
         std::ofstream programaEmCodigo;
+        
+        void pulaLinhas();
+        void criaTabelaSimbolos();
 
-        Analex();
+        void traduzSimbolosEspeciais();
+        void traduzNomes(std::string *token);
+        void traduzNumeros(std::string *token);
+
+        void PROXIMO();
+        void ERRO(std::string atomo);//TEM QUE MUDAR
+        void CODIGO(std::string *token);
+
+        
 
     public:
         void traducao();
-        void PROXIMO();
-        void ERRO();
-        void CODIGO(std::string atomo);
+        Analex();
+        void fechaArq();
+        
 
 };
